27263af09fdba106d72bfcc34db2df8c055f5cda
iBeiCroon <ibeicroon@gmail.com>
2024-08-02 14:54:36 +0800

    init

--- beicroon-service/beicroon-service-admin/beicroon-admin-entity/src/main/java/com/beicroon/service/admin/entity/account/admin/vo/AccountAdminPageVO.java
+++
@@ -1,9 +0,0 @@
-package com.beicroon.service.admin.entity.account.admin.vo;
-
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
-@Data
-@EqualsAndHashCode(callSuper = true)
-public class AccountAdminPageVO extends AccountAdminBaseVO {
-}
---
+++ beicroon-service/beicroon-service-admin/beicroon-service-admin-entity/src/main/java/com/beicroon/service/admin/entity/account/admin/vo/AccountAdminPageVO.java
@@ -0,0 +1,10 @@
+package com.beicroon.service.admin.entity.account.admin.vo;
+
+import com.beicroon.common.admin.entity.admin.vo.AccountAdminBaseVO;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class AccountAdminPageVO extends AccountAdminBaseVO {
+}
