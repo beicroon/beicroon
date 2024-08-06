b460eeb2b6f7118e6e096dfafcf8ea1564966362
iBeiCroon <ibeicroon@gmail.com>
2024-08-06 14:55:01 +0800

    init

---
+++ beicroon-construct/beicroon-construct-auth/src/main/java/com/beicroon/construct/auth/config/AuthConfig.java
@@ -0,0 +1,35 @@
+package com.beicroon.construct.auth.config;
+
+import com.beicroon.construct.auth.interceptor.AuthInterceptor;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@Import(AuthInterceptor.class)
+public class AuthConfig implements WebMvcConfigurer {
+
+    @Resource
+    private AuthInterceptor authHandler;
+
+    private static final List<String> NO_AUTH = Arrays.asList(
+            "/favicon.ico",
+            "/**/*.js",
+            "/**/*.css",
+            "/**/*.html"
+    );
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        InterceptorRegistration interceptorRegistration = registry.addInterceptor(this.authHandler);
+
+        interceptorRegistration.order(1).addPathPatterns("/**").excludePathPatterns(AuthConfig.NO_AUTH);
+    }
+
+}
---
+++ beicroon-construct/beicroon-construct-auth/src/main/java/com/beicroon/construct/auth/interceptor/AuthInterceptor.java
@@ -0,0 +1,57 @@
+package com.beicroon.construct.auth.interceptor;
+
+import com.beicroon.construct.auth.utils.AuthUtils;
+import com.beicroon.construct.constant.HeaderConstant;
+import com.beicroon.construct.entity.AuthUser;
+import com.beicroon.construct.jackson.utils.JsonUtils;
+import com.beicroon.construct.utils.EmptyUtils;
+import com.beicroon.construct.utils.NumberUtils;
+import com.beicroon.construct.utils.UrlUtils;
+import jakarta.annotation.Nonnull;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+public class AuthInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(@Nonnull HttpServletRequest request, @Nonnull HttpServletResponse response, @Nonnull Object handler) throws Exception {
+        // 登录用户信息
+        String user = request.getHeader(HeaderConstant.AUTHORIZATION_USER);
+
+        if (EmptyUtils.isNotEmpty(user)) {
+            AuthUtils.setUser(JsonUtils.toObject(UrlUtils.decode(user), AuthUser.class));
+        }
+
+        // 来源
+        String source = request.getHeader(HeaderConstant.SOURCE);
+
+        if (EmptyUtils.isNotEmpty(source)) {
+            AuthUtils.setSource(UrlUtils.decode(source));
+        }
+
+        // 版本
+        String version = request.getHeader(HeaderConstant.VERSION);
+
+        if (EmptyUtils.isNotEmpty(version)) {
+            AuthUtils.setVersion(UrlUtils.decode(version));
+        }
+
+        // 租户
+        String tenantId = request.getHeader(HeaderConstant.TENANT_ID);
+
+        if (EmptyUtils.isNotEmpty(tenantId)) {
+            AuthUtils.setTenantId(NumberUtils.longOf(UrlUtils.decode(tenantId)));
+        }
+
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void afterCompletion(@Nonnull HttpServletRequest request, @Nonnull HttpServletResponse response, @Nonnull Object handler, Exception ex) {
+        AuthUtils.clear();
+    }
+
+}
