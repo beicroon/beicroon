dcadc1380877aa5a4bcaa491865e49c04a7f5e34
iBeiCroon <ibeicroon@gmail.com>
2024-08-12 14:58:34 +0800

    init

--- beicroon-app/beicroon-app-admin/src/components/elements/ElInput.vue
+++ beicroon-app/beicroon-app-admin/src/components/elements/ElInput.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {computed} from "vue";
+import {computed, ref} from "vue";
 
 const props = defineProps({
   modelValue: {
@@ -23,6 +23,8 @@ const props = defineProps({
   },
 });
 
+const elInput = ref();
+
 const emits = defineEmits(["update:modelValue"]);
 
 const value = computed({
@@ -30,15 +32,19 @@ const value = computed({
     return props.modelValue;
   },
   set(val) {
+    if (elInput.value) {
+      elInput.value.classList.remove("invalid")
+    }
+
     emits("update:modelValue", val);
   }
 })
 </script>
 
 <template>
-  <div class="el-input">
-    <label :class="{required: required}">{{label}}</label>
-    <input type="text" :placeholder="placeholder" v-model="value" />
+  <div class="el-input" :class="{required: required}" ref="elInput">
+    <label>{{ label }}</label>
+    <input class="input-value" type="text" :placeholder="placeholder" v-model="value"/>
   </div>
 </template>
 
@@ -47,19 +53,105 @@ const value = computed({
 
 .el-input {
   gap: 8rem;
-  height: 80rem;
-  width: 340rem;
+  z-index: 1;
+  width: 360rem;
   display: flex;
+  height: 120rem;
+  position: relative;
+  border-radius: 8rem;
+  padding: 8rem 24rem;
   flex-direction: column;
   justify-content: center;
+  transition: all 300ms linear;
+
+  &:focus-within {
+    background-color: @lighter-blue;
+  }
+
+  &.invalid {
+    background-color: @lighter-red;
+
+    &:after {
+      top: 8rem;
+      right: 8rem;
+      color: @white;
+      content: "必填";
+      position: absolute;
+      padding: 4rem 8rem;
+      border-radius: 8rem;
+      background-color: @dangerous;
+    }
+  }
+
+  &.required {
+    label {
+      &:before {
+        top: 0;
+        left: 0;
+        width: 0;
+        height: 0;
+        content: "";
+        font-size: 18rem;
+        font-weight: bold;
+        text-align: center;
+        position: absolute;
+        border-top: 24rem solid @red;
+        border-top-left-radius: 8rem;
+        background-color: transparent;
+        border-right: 24rem solid transparent;
+      }
+
+      &:after {
+        width: 0;
+        height: 0;
+        top: -1rem;
+        left: 1rem;
+        content: "*";
+        color: @yellow;
+        font-size: 18rem;
+        font-weight: bold;
+        position: absolute;
+      }
+    }
+  }
 
   label {
+    z-index: 1;
     display: block;
+    position: relative;
     width: fit-content;
     padding: 8rem 32rem;
     border-radius: 6rem;
     letter-spacing: 4rem;
     background-color: @light-grey;
+
+    &.required:before {
+      top: 0;
+      left: 0;
+      width: 0;
+      height: 0;
+      content: "";
+      font-size: 18rem;
+      font-weight: bold;
+      text-align: center;
+      position: absolute;
+      border-top: 24rem solid @red;
+      border-top-left-radius: 8rem;
+      background-color: transparent;
+      border-right: 24rem solid transparent;
+    }
+
+    &.required:after {
+      width: 0;
+      height: 0;
+      top: -1rem;
+      left: 1rem;
+      content: "*";
+      color: @yellow;
+      font-size: 18rem;
+      font-weight: bold;
+      position: absolute;
+    }
   }
 
   input {
--- beicroon-app/beicroon-app-admin/src/views/index/account/admin/AccountAdminCreate.vue
+++ beicroon-app/beicroon-app-admin/src/views/index/account/admin/AccountAdminCreate.vue
@@ -5,7 +5,9 @@ import ElInput from "@/components/elements/ElInput.vue";
 import ElButton from "@/components/elements/ElButton.vue";
 import {AdminCreateDTO as DTO, create} from "@/https/account/admin.http.ts";
 
-const form = ref<DTO>({});
+const form = ref();
+
+const params = ref<DTO>({});
 
 const loading = ref(false);
 
@@ -18,7 +20,35 @@ function cancel() {
 async function confirm() {
   loading.value = true;
 
-  await create(form.value);
+  const inputs: Element[] = form.value.querySelectorAll(".form-input.required");
+
+  let valid = true;
+
+  for (let i = 0; i < inputs.length; i++) {
+    const elInput = inputs[i];
+
+    const input: HTMLInputElement | null = elInput.querySelector(".input-value");
+
+    if (input == null) {
+      continue;
+    }
+
+    if (!input.value) {
+      valid = false;
+
+      elInput.classList.add("invalid");
+    }
+  }
+
+  if (!valid) {
+    await toast("请填写或选择必填项！", true);
+
+    loading.value = false;
+
+    return;
+  }
+
+  await create(params.value);
 
   await toast("添加成功");
 
@@ -29,12 +59,12 @@ async function confirm() {
 </script>
 
 <template>
-  <form class="create">
+  <form class="create" ref="form">
     <div class="view">
-      <el-input class="form-input" label="账号" placeholder="请输入账号" v-model="form.username"></el-input>
-      <el-input class="form-input" label="昵称" placeholder="请输入昵称" v-model="form.nickname"></el-input>
-      <el-input class="form-input" label="电话" placeholder="请输入电话" v-model="form.phone"></el-input>
-      <el-input class="form-input" label="邮箱" placeholder="请输入邮箱" v-model="form.email"></el-input>
+      <el-input required class="form-input" label="账号" placeholder="请输入账号" v-model="params.username"></el-input>
+      <el-input required class="form-input" label="昵称" placeholder="请输入昵称" v-model="params.nickname"></el-input>
+      <el-input class="form-input" label="电话" placeholder="请输入电话" v-model="params.phone"></el-input>
+      <el-input class="form-input" label="邮箱" placeholder="请输入邮箱" v-model="params.email"></el-input>
     </div>
     <div class="button">
       <el-button class="cancel" @click="cancel" :loading="loading">取消</el-button>
