f72454e3d826df1b882b1c2c107536dc8fdbd318
iBeiCroon <ibeicroon@gmail.com>
2024-09-06 16:10:56 +0800

    init

--- beicroon-app/beicroon-app-admin/src/main.ts
+++ beicroon-app/beicroon-app-admin/src/main.ts
@@ -1,7 +1,7 @@
 import {createApp} from "vue";
 
 import router from "@/router.ts";
-import App from "@/apps/Main.vue";
+import App from "@/apps/App.vue";
 import {AppSelectorEnums} from "@/enums/default-enums.ts";
 
 const app = createApp(App)
