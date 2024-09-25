ae131aed31dd1def7202cfdd383eca3ff5c4fd0a
iBeiCroon <ibeicroon@gmail.com>
2024-09-25 17:40:18 +0800

    init

--- beicroon-app/beicroon-app-admin/src/modules/default/role/ResourceRoleDetail.vue
+++ beicroon-app/beicroon-app-admin/src/modules/default/role/ResourceRoleDetail.vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
 import {onMounted, ref} from "vue";
-import BeicroonForm from "@/components/BeicroonForm.vue";
-import BeicroonInput from "@/components/BeicroonInput.vue";
-import BeicroonButton from "@/components/BeicroonButton.vue";
-import BeicroonLoading from "@/components/BeicroonLoading.vue";
-import BeicroonLineVertical from "@/components/BeicroonLineVertical.vue";
+import ResourceRoleDetailView from "./ResourceRoleDetailView.vue";
 import {detail, ResourceRoleDetailVO as VO} from "@/request/resource-role.http.ts";
 
 type Props = {
@@ -29,20 +25,7 @@ onMounted(async () => {
 </script>
 
 <template>
-  <beicroon-form class="beicroon-dialog-view">
-    <div class="beicroon-dialog-main beicroon-dialog-input" v-if="form">
-      <beicroon-input disabled class="column" label="编码" placeholder="编码" v-model="form.code"></beicroon-input>
-      <beicroon-input disabled class="column" label="名称" placeholder="名称" v-model="form.name"></beicroon-input>
-      <beicroon-input disabled class="column" label="备注" placeholder="备注" v-model="form.remark"></beicroon-input>
-    </div>
-    <div class="beicroon-dialog-loading" v-else>
-      <beicroon-loading fill="#b3e5fc" width="100" height="100"></beicroon-loading>
-    </div>
-    <beicroon-line-vertical></beicroon-line-vertical>
-    <div class="beicroon-dialog-button">
-      <beicroon-button class="block primary" label="关闭" @click="handleCancel"></beicroon-button>
-    </div>
-  </beicroon-form>
+  <resource-role-detail-view :data="form" @cancel="handleCancel"></resource-role-detail-view>
 </template>
 
 <style scoped lang="less">
---
+++ beicroon-app/beicroon-app-admin/src/modules/default/role/ResourceRoleDetailView.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import BeicroonForm from "@/components/BeicroonForm.vue";
+import BeicroonInput from "@/components/BeicroonInput.vue";
+import BeicroonButton from "@/components/BeicroonButton.vue";
+import BeicroonLoading from "@/components/BeicroonLoading.vue";
+import BeicroonLineVertical from "@/components/BeicroonLineVertical.vue";
+import {ResourceRoleDetailVO as VO} from "@/request/resource-role.http.ts";
+
+type Props = {
+  data: VO | null,
+};
+
+defineProps<Props>();
+
+const emits = defineEmits(["cancel"]);
+
+async function handleCancel() {
+  emits("cancel");
+}
+</script>
+
+<template>
+  <beicroon-form class="beicroon-dialog-view">
+    <div class="beicroon-dialog-main beicroon-dialog-input" v-if="data">
+      <beicroon-input disabled class="column" label="编码" placeholder="编码" v-model="data.code"></beicroon-input>
+      <beicroon-input disabled class="column" label="名称" placeholder="名称" v-model="data.name"></beicroon-input>
+      <beicroon-input disabled class="column" label="备注" placeholder="备注" v-model="data.remark"></beicroon-input>
+    </div>
+    <div class="beicroon-dialog-loading" v-else>
+      <beicroon-loading fill="#b3e5fc" width="100" height="100"></beicroon-loading>
+    </div>
+    <beicroon-line-vertical></beicroon-line-vertical>
+    <div class="beicroon-dialog-button">
+      <beicroon-button class="block primary" label="关闭" @click="handleCancel"></beicroon-button>
+    </div>
+  </beicroon-form>
+</template>
+
+<style scoped lang="less">
+</style>
