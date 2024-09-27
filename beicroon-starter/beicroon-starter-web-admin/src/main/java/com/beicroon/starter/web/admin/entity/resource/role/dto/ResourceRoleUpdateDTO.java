7330e63ac2008ca818b9f82e0ad17c047a38ca8e
iBeiCroon <ibeicroon@gmail.com>
2024-09-27 10:23:38 +0800

    init

--- beicroon-starter/beicroon-starter-web-admin/src/main/java/com/beicroon/starter/web/admin/entity/resource/role/dto/ResourceRoleUpdateDTO.java
+++ beicroon-starter/beicroon-starter-web-admin/src/main/java/com/beicroon/starter/web/admin/entity/resource/role/dto/ResourceRoleUpdateDTO.java
@@ -3,6 +3,7 @@ package com.beicroon.starter.web.admin.entity.resource.role.dto;
 import com.beicroon.construct.annotation.ApiModel;
 import com.beicroon.construct.annotation.ApiModelProperty;
 import com.beicroon.construct.entity.UpdateDTO;
+import com.beicroon.construct.validator.annotation.NeedValue;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -11,9 +12,11 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 public class ResourceRoleUpdateDTO extends UpdateDTO {
 
+    @NeedValue(message = "编码[code]不能为空")
     @ApiModelProperty(name = "编码")
     private String code;
 
+    @NeedValue(message = "名称[name]不能为空")
     @ApiModelProperty(name = "名称")
     private String name;
 
