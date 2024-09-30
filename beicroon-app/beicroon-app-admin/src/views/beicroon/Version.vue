88eb115e1b8544d92dba5c07720c15962de01b35
iBeiCroon <ibeicroon@gmail.com>
2024-09-30 16:43:32 +0800

    init

---
+++ beicroon-app/beicroon-app-admin/src/views/beicroon/Version.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="beicroon-version">
+  <span>系统版本号：V1.0.0</span>
+</div>
+</template>
+
+<style lang="less">
+.beicroon-version {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  font-size: 32rem;
+  font-weight: bold;
+  align-items: center;
+  justify-content: center;
+}
+</style>
