78fc9b21aefe12a25205f4815e38808bc573a12f
iBeiCroon <ibeicroon@gmail.com>
2024-10-08 11:50:01 +0800

    init

--- beicroon-starter/beicroon-starter-web-admin/src/main/resources/migration/V1.0.0_feature.sql
+++ beicroon-starter/beicroon-starter-web-admin/src/main/resources/migration/V1.0.0_feature.sql
@@ -147,31 +147,31 @@ create table `admin_account_admin_role_relation`
 
 INSERT INTO `admin_account_admin`
 (`id`, `code`, `name`, `username`, `password`, `root_flag`, `creator_name`, `modifier_name`)
-VALUES (1825706731063808302, 'root', 'root', 'root', 'b13bc5fefc72a6a06c6c172d227f5c39967cdbbcdf8f20b5fc39f175bfb608a5',
-        'true', 'root', 'root'),
-       (1825706731063808332, 'admin', 'admin', 'admin',
-        'faf1f45a58caa5b6bdb451adb8a71dfe697472e5c85516ec40fde6f989719610', 'false', 'root', 'root');
+VALUES (1825706731063808302, 'ROOT', '超级管理员', 'root', 'b13bc5fefc72a6a06c6c172d227f5c39967cdbbcdf8f20b5fc39f175bfb608a5',
+        'true', '超级管理员', '超级管理员'),
+       (1825706731063808332, 'ADMIN', '管理员', 'admin',
+        'faf1f45a58caa5b6bdb451adb8a71dfe697472e5c85516ec40fde6f989719610', 'false', '超级管理员', '超级管理员');
 
 INSERT INTO `admin_resource_menu`
 (`id`, `parent_id`, `parent_code`, `parent_name`, `code`, `name`, `path`, `creator_name`, `modifier_name`)
-VALUES (1825706731063808002, 0, '', '', 'ACCOUNT_CENTER', '账号中心', '/account/center', 'root', 'root'),
+VALUES (1825706731063808002, 0, '', '', 'ACCOUNT_CENTER', '账号中心', '/account/center', '超级管理员', '超级管理员'),
        (1825707094248591361, 1825706731063808002, 'ACCOUNT_CENTER', '账号中心', 'ACCOUNT_MANAGER', '账号管理',
-        '/account/manager', 'root', 'root'),
+        '/account/manager', '超级管理员', '超级管理员'),
        (1825707293197012994, 1825707094248591361, 'ACCOUNT_MANAGER', '账号管理', 'ACCOUNT_ADMIN', '后台账号',
-        '/account/admin', 'root', 'root'),
-       (1825707485132558337, 0, '', '', 'SETTING_CENTER', '设置中心', '/setting/center', 'root', 'root'),
+        '/account/admin', '超级管理员', '超级管理员'),
+       (1825707485132558337, 0, '', '', 'SETTING_CENTER', '设置中心', '/setting/center', '超级管理员', '超级管理员'),
        (1825707757028315138, 1825707485132558337, 'SETTING_CENTER', '设置中心', 'SETTING_SYSTEM', '系统设置',
-        '/setting/system', 'root', 'root'),
+        '/setting/system', '超级管理员', '超级管理员'),
        (1825707969033605121, 1825707757028315138, 'SETTING_SYSTEM', '系统设置', 'RESOURCE_MENU', '菜单管理',
-        '/resource/menu', 'root', 'root'),
+        '/resource/menu', '超级管理员', '超级管理员'),
        (1825707969033606521, 1825707757028315138, 'SETTING_SYSTEM', '系统设置', 'RESOURCE_ROLE', '角色管理',
-        '/resource/role', 'root', 'root');
+        '/resource/role', '超级管理员', '超级管理员');
 
 INSERT INTO `admin_resource_role`
 (`id`, `code`, `name`, `remark`, `root_flag`, `creator_name`, `modifier_name`)
-VALUES (1825706731063808102, 'ROOT', '超级管理员', '超级管理员', 'true', 'root', 'root'),
-       (1825706731063809202, 'ADMIN', '管理员', '管理员', 'false', 'root', 'root');
+VALUES (1825706731063808102, 'ROOT', '超级管理员', '超级管理员', 'true', '超级管理员', '超级管理员'),
+       (1825706731063809202, 'ADMIN', '管理员', '管理员', 'false', '超级管理员', '超级管理员');
 
 insert into `admin_account_admin_role_relation`
     (`admin_id`, `role_id`, `creator_name`, `modifier_name`)
-values (1825706731063808302, 1825706731063808102, 'root', 'root');
+values (1825706731063808302, 1825706731063808102, '超级管理员', '超级管理员');
