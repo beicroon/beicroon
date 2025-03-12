bf0626ec2c126f88b8d441f8ab0e8603e23f8e52
iBeiCroon <ibeicroon@gmail.com>
2025-03-12 10:50:33 +0800

    init

--- beicroon-starter/beicroon-starter-generic/src/main/java/com/beicroon/starter/generic/BeicroonProjectGenericApplication.java
+++ beicroon-starter/beicroon-starter-generic/src/main/java/com/beicroon/starter/generic/BeicroonProjectGenericApplication.java
@@ -9,11 +9,11 @@ public class BeicroonProjectGenericApplication {
 
     private static final String SERVICE_PREFIX = "beicroon-service";
 
-    private static final String MODULE_NAME = "order";
+    private static final String MODULE_NAME = "banner";
 
     private static final String BASE_PACKAGE = "com.beicroon.service";
 
-    private static final int SERVER_PORT = 8300;
+    private static final int SERVER_PORT = 8080;
 
     public static void main(String[] args) {
         GenericStructure structure = new GenericStructure(
